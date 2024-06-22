7635d7950b4e6c8360a3dfba6b251c093f0588be
ReturnZeroGirl <phc1024@outlook.com>
2024-06-22 11:24:36 +0800

    2.1

--- mainwin.py
+++ mainwin.py
@@ -44,6 +44,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "PyQt-clock"))
         self.label.setText(_translate("MainWindow", "当前时间"))
         self.label_2.setText(_translate("MainWindow", "当前日期"))
